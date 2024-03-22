6de4fb1887ef2752f368ba2e41a7738f8699b9da
samuelsouzzza <rssamuel17@gmail.com>
2024-03-21 22:22:50 -0300

    Fundo da Home adicionado

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import { MenuNavHome } from '@/components/MenuNavHome/MenuNavHome';
+import styles from './page.module.css';
 
 export const metadata: Metadata = {
   title: 'Veridi Oculi • Home',
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function homePage() {
-  return <MenuNavHome />;
+  return (
+    <div className={styles.container}>
+      <MenuNavHome />
+    </div>
+  );
 }
