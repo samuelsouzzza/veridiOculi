f10087ca2b4b840755ad3c2913661b4451235215
samuelsouzzza <rssamuel17@gmail.com>
2024-03-31 06:08:12 -0300

    Tabela de histórico criada

--- src/app/historic/page.tsx
+++ src/app/historic/page.tsx
@@ -11,7 +11,51 @@ export default function historicPage() {
   return (
     <div className={styles.container}>
       <MenuNavHome activeRoute='historic' />
-      Histórico
+      <div className={styles.view}>
+        <h1 className='title'>Meu histórico</h1>
+        <div className={styles.boxTable}>
+          <div className={styles.rowHead}>
+            <div className={styles.colHead}>Id</div>
+            <div className={styles.colHead}>Data</div>
+            <div className={styles.colHead}>Foco</div>
+            <div className={styles.colHead}>Qtd Imgs</div>
+            <div className={styles.colHead}>Status</div>
+            <div className={styles.colHead}>Relatório</div>
+          </div>
+          <div className={styles.rowBody}>
+            <div className={styles.colBody}>3515485</div>
+            <div className={styles.colBody}>28/06/2003 10:00</div>
+            <div className={styles.colBody}>Ypê Rosa</div>
+            <div className={styles.colBody}>15</div>
+            <div className={styles.colBody}>Concluído</div>
+            <div className={styles.colBody}>📄</div>
+          </div>
+          <div className={styles.rowBody}>
+            <div className={styles.colBody}>3515485</div>
+            <div className={styles.colBody}>28/06/2003 10:00</div>
+            <div className={styles.colBody}>Ypê Rosa</div>
+            <div className={styles.colBody}>15</div>
+            <div className={styles.colBody}>Concluído</div>
+            <div className={styles.colBody}>📄</div>
+          </div>
+          <div className={styles.rowBody}>
+            <div className={styles.colBody}>3515485</div>
+            <div className={styles.colBody}>28/06/2003 10:00</div>
+            <div className={styles.colBody}>Ypê Rosa</div>
+            <div className={styles.colBody}>15</div>
+            <div className={styles.colBody}>Concluído</div>
+            <div className={styles.colBody}>📄</div>
+          </div>
+          <div className={styles.rowBody}>
+            <div className={styles.colBody}>3515485</div>
+            <div className={styles.colBody}>28/06/2003 10:00</div>
+            <div className={styles.colBody}>Ypê Rosa</div>
+            <div className={styles.colBody}>15</div>
+            <div className={styles.colBody}>Concluído</div>
+            <div className={styles.colBody}>📄</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
