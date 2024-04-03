a5377ad1ab0d15024dbb86f7d4aba2d2a58b05d2
samuelsouzzza <rssamuel17@gmail.com>
2024-04-03 12:10:06 -0300

    Reversão do HistoricView adicionada

--- src/components/HistoricView/HistoricView.tsx
+++ src/components/HistoricView/HistoricView.tsx
@@ -2,31 +2,38 @@
 import styles from './HistoricView.module.css';
 import { IReports } from '@/@types/@types';
 import Link from 'next/link';
+import React from 'react';
 
 type HistoricViewProps = {
   data: IReports[];
 };
 
 export function HistoricView({ data }: HistoricViewProps) {
+  const [historic, setHistoric] = React.useState<IReports[]>(data);
+
+  function reverseHistoric() {
+    setHistoric((historic) => historic.toReversed());
+  }
+
   return (
     <div className={styles.view}>
       <h1 className='subtitle'>Meu histórico</h1>
       <div className={styles.boxTable}>
-        <div className={styles.rowHead}>
+        <div className={styles.rowHead} onClick={reverseHistoric}>
           <div className={styles.colHead}>Id</div>
           <div className={styles.colHead}>Data</div>
           <div className={styles.colHead}>Espécie Foco</div>
           <div className={styles.colHead}>Acurácia</div>
           <div className={styles.colHead}>Status</div>
         </div>
-        {data.map((d) => {
+        {historic.map((h) => {
           return (
-            <Link href={`historic/${d.id}`} className={styles.linkRow}>
-              <div key={d.id} className={styles.rowBody}>
-                <div className={styles.colBody}>{d.id}</div>
-                <div className={styles.colBody}>{d.dt_report}</div>
-                <div className={styles.colBody}>{d.data.species_name}</div>
-                <div className={styles.colBody}>{d.data.accuracy}</div>
+            <Link href={`historic/${h.id}`} className={styles.linkRow}>
+              <div key={h.id} className={styles.rowBody}>
+                <div className={styles.colBody}>{h.id}</div>
+                <div className={styles.colBody}>{h.dt_report}</div>
+                <div className={styles.colBody}>{h.data.species_name}</div>
+                <div className={styles.colBody}>{h.data.accuracy}</div>
                 <div className={styles.colBody}>Concluído</div>
               </div>
             </Link>
